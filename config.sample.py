ea511d41cf10f365ae7215f55f8ea95df2380aab
JongPark <jongpark1234@naver.com>
2023-12-27 15:34:18 +0900

    Initial Commit

---
+++ config.sample.py
@@ -0,0 +1,4 @@
+LOGIN_URL = 'https://lng-tgk-aime-gw.am-all.net/common_auth/login?site_id=chuniex&redirect_url=https://chunithm-net-eng.com/mobile/&back_url=https://chunithm.sega.com/'
+
+TWITTER_ID = 'your_twitter_id'
+TWITTER_PW = 'your_twitter_pw'
---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import common, types, webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.options import *
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+
+from config import *
+
+driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+
+
+driver.get(LOGIN_URL)
+driver.find_element(By.CLASS_NAME, 'c-button--openid--twitter').click()
+driver.find_element(By.ID, 'username_or_email').send_keys(TWITTER_ID)
+driver.find_element(By.ID, 'password').send_keys(TWITTER_PW)
+driver.find_element(By.ID, 'allow').click()
+driver.get('https://chunithm-net-eng.com/mobile/friend/levelVs')
+Select(driver.find_element(By.NAME, 'level')).select_by_visible_text('LEVEL 15')
+Select(driver.find_element(By.NAME, 'friend')).select_by_index(1)
+driver.find_element(By.CLASS_NAME, 'btn_battle').click()
+musiclist = driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div[3]/div[2]/div[5]')
+for music in musiclist.find_elements(By.XPATH, './*')[1:]:
+    print(f'{music.find_element(By.XPATH, './div[1]/div[1]').text}: {music.find_element(By.XPATH, './div[2]/div[3]').text}')
+driver.quit()
+
