847036fa296502d28d32b97e468094b0abb14399
JongPark <jongpark1234@naver.com>
2024-12-12 21:36:52 +0900

    Change chart getting Path

--- server/getChart.py
+++ server/getChart.py
@@ -2,5 +2,5 @@ from requests import get
 
 from fetchURL import *
 
-with open('chart.json', 'w', encoding='utf-8') as f:
+with open('server/chart.json', 'w', encoding='utf-8') as f:
     f.write(get(CHART_DATA_FETCH_URL).text)
